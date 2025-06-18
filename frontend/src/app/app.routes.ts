72b861228f75cd530e303f284143f2d0119aca89
Raaaaaaaaaj <raj2222ghosh@gmail.com>
2025-06-19 00:16:31 +0530

    Done major features in search component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SearchresultComponent } from './feature/searchresult/searchresult.component';
+import { SearchComponent } from './feature/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'search', pathMatch: 'full' },
+    { path: 'search', component: SearchComponent},
+    { path: 'searchResult', component: SearchresultComponent }
+];
--- frontend/src/app/feature/search/search.component.ts
+++ frontend/src/app/feature/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DatePicker } from 'primeng/datepicker';
@@ -8,11 +8,34 @@ import { DatePicker } from 'primeng/datepicker';
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
   dates: Date[] | undefined;
   // Flag value
   showPromo : boolean = false;
 
+  ngOnInit(): void {
+      console.log("Initialized")
+  }
+
+  // Adult Count
+  adultCount: number = 1;
+  increment(){
+    this.adultCount +=1
+  };
+
+  decrement(){
+    if(this.adultCount > 1){
+      this.adultCount -=1;
+    };
+  };
+
+  handleKeyControl(event: KeyboardEvent){
+    console.log("function hit")
+    if(event.key === 'ArrowUp' ) this.increment();
+    else if(event.key === 'ArrowDown') this.decrement();
+  }
+
+  // Promo input show
   showPromoInput(){
    this.showPromo = true;
   }
