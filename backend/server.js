0f49aed83b3f47e2cbc6eaf656503b0db8e29c03
Raaaaaaaaaj <raj2222ghosh@gmail.com>
2025-07-28 23:28:24 +0530

    Done
    Done

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,21 @@ const dotenv = require('dotenv');
 const connecToDb = require('./config/db');
 const authRoute = require('./routes/auth.routes')
 const cors = require('cors');
+
 dotenv.config();
+
 const port = process.env.PORT || 5000;
+
 connecToDb();
+
 const app = express();
+
 app.use(express.json());
+
 app.use(cors());
+
 app.use('/api', authRoute);
+
 app.listen(port, () => {
     console.log("Server Running On: ", port);
 })
