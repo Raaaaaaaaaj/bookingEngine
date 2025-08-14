fb542b199618de56f4f8d12d3920f5f639fc02ed
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2025-08-14 18:30:05 +0530

    envo

--- frontend/src/app/feature/admin/login/login.component.ts
+++ frontend/src/app/feature/admin/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { environment } from '../../../../../environments/environment';
+import { environment } from '../../../../../environment/environment';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
