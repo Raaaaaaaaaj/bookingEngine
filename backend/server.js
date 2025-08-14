131c65bb44885649c472c4ce4d1274f51c280738
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2025-08-14 17:17:52 +0530

    Cors Issue solve

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://mybookingengine.netlify.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.send('Backend is running...');
