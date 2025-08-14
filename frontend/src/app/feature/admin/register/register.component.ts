12e03d17f86ed17b0e831b1726498155c15a6698
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2025-08-14 23:54:52 +0530

    d

--- frontend/src/app/feature/admin/register/register.component.ts
+++ frontend/src/app/feature/admin/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { environment } from '../../../../environment/environment';
+import { environment } from '../../../../environments/environment';
 // import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
