ad4b3392105abe7760cf1081c9e8082f0b1199b3
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2025-08-18 13:07:06 +0530

    Dashboard component
    Working on dashboard component

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -11,7 +11,8 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { MessageService } from 'primeng/api';
-import Aura from '@primeng/themes/aura';
+// import Aura from '@primeng/themes/aura';
+import Lara from '@primeng/themes/lara';
 import {
   provideClientHydration,
   withEventReplay,
@@ -29,8 +30,14 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     providePrimeNG({
+      // theme: {
+      //   preset: Aura,
+      // },
       theme: {
-        preset: Aura,
+        preset: Lara,
+        options: {
+          darkModeSelector: false // ensure light
+        }
       },
     }),
     MessageService,
