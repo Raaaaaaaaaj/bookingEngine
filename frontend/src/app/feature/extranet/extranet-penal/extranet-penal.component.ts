875440c163866c5cc86857dc8d7d61b7088b5de0
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2025-08-20 17:23:39 +0530

    Sidepanel
    Sidepanel rendered conditionally

--- frontend/src/app/feature/extranet/extranet-penal/extranet-penal.component.ts
+++ frontend/src/app/feature/extranet/extranet-penal/extranet-penal.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { OnboardSidePanelComponent } from '../../../shared/components/onboard-side-panel/onboard-side-panel.component';
+import { SidePanelComponent } from '../../../shared/components/side-panel/side-panel.component';
 import { PropertiesComponent } from './properties/properties.component';
 @Component({
   selector: 'app-extranet-penal',
-  imports: [OnboardSidePanelComponent, PropertiesComponent],
+  imports: [SidePanelComponent, PropertiesComponent],
   templateUrl: './extranet-penal.component.html',
   styleUrl: './extranet-penal.component.css'
 })
--- frontend/src/app/shared/components/side-panel/side-panel.component.ts
+++ frontend/src/app/shared/components/side-panel/side-panel.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-panel',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './side-panel.component.html',
   styleUrl: './side-panel.component.css'
 })
 export class SidePanelComponent {
+  currentUrl: string;
 
+  constructor(private router: Router) {
+    this.currentUrl = this.router.url;
+    this.router.events.subscribe(() => {
+      this.currentUrl = this.router.url;
+    });
+  }
+
+  isExtranet() {
+    return this.currentUrl.includes('/extranet');
+  }
+  isAdmin() {
+    return this.currentUrl.includes('/admin')
+  }
 }
