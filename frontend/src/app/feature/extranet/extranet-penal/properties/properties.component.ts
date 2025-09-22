3282f8e3f2cefe848d46fb6e67f5ba308190ef7c
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2025-09-22 18:36:50 +0530

    Changes
    Changes

--- frontend/src/app/feature/extranet/extranet-penal/properties/properties.component.ts
+++ frontend/src/app/feature/extranet/extranet-penal/properties/properties.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Auth } from '../../../../core/services/auth';
 import { OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TabsModule } from 'primeng/tabs';
 // import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-properties',
-  imports: [RouterModule],
+  imports: [RouterModule, TabsModule, CommonModule],
   templateUrl: './properties.component.html',
   styleUrl: './properties.component.css'
 })
@@ -14,4 +16,10 @@ export class PropertiesComponent implements OnInit {
   ngOnInit() {
     this.authService.loadFromLocalStorage()
   }
+  tabs = [
+    { route: 'dashboard', label: 'Dashboard', icon: 'pi pi-home' },
+    { route: 'transactions', label: 'Transactions', icon: 'pi pi-chart-line' },
+    { route: 'products', label: 'Products', icon: 'pi pi-list' },
+    { route: 'messages', label: 'Messages', icon: 'pi pi-inbox' }
+  ];
 }
