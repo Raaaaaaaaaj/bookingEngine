ea85a6d5d8bd48f91dfda1c1d944f411b2583c84
Raaaaaaaaaj <raj2222ghosh@gmail.com>
2025-10-14 21:37:00 +0530

    Master Stepper
    Master stepper initialized freshly on loop

--- frontend/src/app/feature/extranet/create-property/create-property.component.ts
+++ frontend/src/app/feature/extranet/create-property/create-property.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { StepperModule } from 'primeng/stepper';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-create-property',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, StepperModule, CommonModule],
   templateUrl: './create-property.component.html',
   styleUrls: ['./create-property.component.css']
 })
 export class CreatePropertyComponent {
+  currentStep = 1;
+  steps = ["Basic Info", "Amenities", "Rooms", "Photos", "Policies"];
 
+  nextStep() {
+    if (this.currentStep < this.steps.length) {
+      this.currentStep++;
+    }
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
 }
