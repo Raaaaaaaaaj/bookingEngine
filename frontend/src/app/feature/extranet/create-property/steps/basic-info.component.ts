2dc9fdd8aab25a34019cfabefc4aae016656ef98
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2025-10-21 13:40:40 +0530

    Hotel Rating implemented

--- frontend/src/app/feature/extranet/create-property/steps/basic-info.component.ts
+++ frontend/src/app/feature/extranet/create-property/steps/basic-info.component.ts
@@ -2,10 +2,10 @@ import { Component, Input } from "@angular/core";
 import { FormGroup, ReactiveFormsModule, FormBuilder, Validators } from "@angular/forms";
 import { CommonModule } from '@angular/common';
 import { InputComponent } from "../../../../shared/components/input/input.component";
-// import { SelectComponent } from "../../../../shared/components/select/select.component";
+import { SelectComponent } from "../../../../shared/components/select/select.component";
 @Component({
   selector: "app-basic-info",
-  imports: [ReactiveFormsModule, CommonModule, InputComponent],
+  imports: [ReactiveFormsModule, CommonModule, InputComponent, SelectComponent],
   template: `
   <div class="row d-flex align-items-center flex-column justify-content-center">
   <div class="stepHeader text-center text-decoration-underline">
@@ -16,7 +16,7 @@ import { InputComponent } from "../../../../shared/components/input/input.compon
       class="row basicInfoForm mt-4"
     >
       <div class="col-md-4 text-left">
-        <p><b>1. Please fill Up your property name</b> <br>
+        <p><b>1. Property name</b> <br>
           (It will be visible on MMt)
         </p>
       </div>
@@ -29,18 +29,27 @@ import { InputComponent } from "../../../../shared/components/input/input.compon
         ></app-input>
       </div>
 
-
       <div class="col-md-4 text-left">
-        <p><b>1. Please fill Up your property name</b> <br>
-          (It will be visible on MMt)
+        <p><b>2. Hotel Star Rating</b> <br>
         </p>
       </div>
-      <div class="col-md-8">
-        <app-input
-          id="hotelName"
-          type="text"
-          label="Hotel Name"
-          [required]="true"
+      <div class="col-md-8 mb-4">
+        <app-select 
+          id="hotelStarRating"
+          [options]="starRatingOptions"
+          arealabel="Select Star Rating"
+        ></app-select>
+      </div>
+      <hr>
+
+      <div class="col-md-4 text-left">
+        <p><b>3. Email Id</b> <br>
+        </p>
+      </div>
+      <div class="col-md-8 mb-4">
+        <app-input 
+          id="hotelStarRating"
+          arealabel="Select Star Rating"
         ></app-input>
       </div>
     </form>
@@ -55,5 +64,13 @@ import { InputComponent } from "../../../../shared/components/input/input.compon
 export class BasicInfoComponent {
   @Input() parentForm!: FormGroup;
   @Input() title!: string;
-  basicInfoForm!: FormGroup
+  basicInfoForm!: FormGroup;
+
+  starRatingOptions: any = [
+    { label: "1 Star", value: 1 },
+    { label: "2 Star", value: 2 },
+    { label: "3 Star", value: 3 },
+    { label: "4 Star", value: 4 },
+    { label: "5 Star", value: 5 },
+  ]
 }
---
+++ frontend/src/app/shared/components/textarea/textarea.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textarea',
+  imports: [],
+  template: ``,
+  styles: `` 
+})
+export class TextareaComponent {
+
+}
